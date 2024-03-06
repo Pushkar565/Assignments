85e2e60a0488053c30ffd1384dd95d1c220e56d0
Pushkar kumar <kpushkar56@gmail.com>
2024-03-06 11:45:48 +0530

    done

--- 1 march/Displaying User Data/index.js
+++ 1 march/Displaying User Data/index.js
@@ -1,21 +1,42 @@
-let userData = document.getElementById('FetchBtn')
+document.getElementById('fetchBtn').addEventListener('click', function(){
+  fetch('https://reqres.in/api/users')
+    .then(response => response.json())
+    .then(data => {
+      displayUsers(data.data);
+    })
+    .catch(error => {
+      console.error('Error fetching users:', error);
+    });
+});
 
-userData.addEventListener('click', function(){
 
-  let fetch_url = fetch('https://reqres.in/api/users')
-  fetch_url.then(function(res){
-    return res.json()
-  })
-  
-  .then(function(res){
-    console.log(res.data)
-  })
-  
-  
-})
+function displayUsers(users) {
+  const usersContainer = document.getElementById('usersContainer');
+  usersContainer.innerHTML = '';
 
-function displayData(){
-  let div = document.createElement('div')
-  
+  users.forEach(user => {
+    const userCard = document.createElement('div');
+    userCard.classList.add('user-card');
+
+    const avatar = document.createElement('img');
+    avatar.src = user.avatar;
+    avatar.alt = user.first_name + ' ' + user.last_name;
+    avatar.classList.add('user-avatar');
+
+    const userInfo = document.createElement('div');
+    userInfo.classList.add('user-info');
+
+    const userName = document.createElement('h3');
+    userName.textContent = user.first_name + ' ' + user.last_name;
+
+    const userEmail = document.createElement('p');
+    userEmail.textContent = user.email;
+
+    userInfo.appendChild(avatar);
+    userInfo.appendChild(userName);
+    userInfo.appendChild(userEmail);
+
+    userCard.appendChild(userInfo);
+    usersContainer.appendChild(userCard);
+  });
 }
-
